e3479a363a7911e4759a6984943693885fa1f6da
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2024-08-23 22:45:34 +0400

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/about-us" element={<div>About Us</div>} />
+        <Route path="/services" element={<div>Services</div>} />
+        <Route path="/use-cases" element={<div>Use Cases</div>} />
+        <Route path="/pricing" element={<div>Pricing</div>} />
+        <Route path="/blog" element={<div>Blog</div>} />
+        <Route path="/request-quote" element={<div>Request Quote</div>} />
+        <Route path="/" element={<div>Home</div>} />
+      </Routes>
+    </Router>
   );
 }
 
