e4a8a24fbe8eb16a60c2f093f7dede39304122e6
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2024-08-27 18:13:08 +0400

    Added Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,42 @@
 import React from "react";
 import "../CSS/Footer.css"; // Import the CSS for styling
+import logo from "../assets/logo1.svg"; // Import the logo image
+import linkedIn from "../assets/socials/linkedin.svg";
+import facebook from "../assets/socials/facebook.svg";
+import twitter from "../assets/socials/twitter.svg"; 
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
+        <div className="footer_logo_nav">
         <div className="footer-brand">
-          <img src="/logo.png" alt="Positivus" className="footer-logo" />
-          <span>Positivus</span>
+          <img src={logo} alt="Positivus" className="footer-logo" />
         </div>
-        <nav className="footer-nav">
-          <a href="/about-us">About us</a>
-          <a href="/services">Services</a>
-          <a href="/use-cases">Use Cases</a>
-          <a href="/pricing">Pricing</a>
-          <a href="/blog">Blog</a>
+    <nav className="footer-nav">
+          <a href="#about-us">About us</a>
+          <a href="#services">Services</a>
+          <a href="#use-cases">Use Cases</a>
+          <a href="#pricing">Pricing</a>
+          <a href="#blog">Blog</a>
         </nav>
+        <div className="footer-social">
+          <a href="https://linkedin.com">
+            <img src={linkedIn} alt="LinkedIn" />
+          </a>
+          <a href="https://facebook.com">
+            <img src={facebook} alt="Facebook" />
+          </a>
+          <a href="https://twitter.com">
+            <img src={twitter} alt="Twitter" />
+          </a>
+        </div>
+        </div>
+        <div className="contact_sub">
         <div className="footer-contact">
-          <h4>Contact us:</h4>
+            <div className="footer_head_title">
+                          <h4>Contact us:</h4>
+            </div>
           <p>Email: info@positivus.com</p>
           <p>Phone: 555-567-8901</p>
           <p>Address: 1234 Main St, Moonstone City, Stardust State 12345</p>
@@ -26,16 +45,6 @@ const Footer = () => {
           <input type="email" placeholder="Email" className="footer-input" />
           <button className="footer-button">Subscribe to news</button>
         </div>
-        <div className="footer-social">
-          <a href="https://linkedin.com">
-            <img src="/linkedin.png" alt="LinkedIn" />
-          </a>
-          <a href="https://facebook.com">
-            <img src="/facebook.png" alt="Facebook" />
-          </a>
-          <a href="https://twitter.com">
-            <img src="/twitter.png" alt="Twitter" />
-          </a>
         </div>
       </div>
       <div className="footer-bottom">
